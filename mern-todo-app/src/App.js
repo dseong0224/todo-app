847fce036093316ce6eaa7bd5f19ec5b05de6084
Daniel Seong <dseong0224@gmail.com>
2019-12-20 23:55:21 -0800

    integrated react router to layout page installed depenencies installed bootstrap

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -1,15 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css';
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h2>TODO APP</h2>
-      </div>
-    )
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a
+              class="navbar-brand"
+              href="http://danielcseong.com"
+              target="_blank"
+            >
+              <img src="https://cdn.onlinewebfonts.com/svg/img_550709.png" width="30" height="30" alt="CodingTheSmartWay.com" />
+            </a>
+            <Link to="/" className="navbar-brand">
+              MERN-Stack Todo App
+            </Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">
+                    Create Todo
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br />
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
+    );
   }
 }
 
