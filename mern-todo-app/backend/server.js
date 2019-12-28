7922c81c33dd79b0684ddb36adbcb726e211da9a
Daniel Seong <dseong0224@gmail.com>
2019-12-27 17:10:47 -0800

    created express routes for get todo list get individual todo item by id

--- mern-todo-app/backend/server.js
+++ mern-todo-app/backend/server.js
@@ -1,21 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const todoRoutes = express.Router();
 
 const PORT = 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true, useUnifiedTopology: true } );
+mongoose.connect("mongodb://127.0.0.1:27017/todos", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const connection = mongoose.connection;
 
-connection.once('open', function (){
-  console.log("MongoDB database connection established successfully")
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
 });
 
-app.listen(PORT, function(){
-  console.log(`Server is running on port: ${PORT}`)
+app.use("/todos", todoRoutes);
+
+todoRoutes.route("/").get((req, res) => {
+  Todo.find((err, todos) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+todoRoutes.route('/:id').get((req,res) => {
+  let id = req.params.id;
+
+  Todo.findById(id, (err, todo) => {
+    res.json(todo);
+})
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
